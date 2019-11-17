a0688891954955952ea3d1509e53f4d09cbfce08
Laszlo Bajusz <martenq1337@gmail.com>
2019-11-17 14:36:27 +0100

    Added: blue and red flashing light to the police car

--- Assets/Scripts/PoliceCarEmission.cs
+++ Assets/Scripts/PoliceCarEmission.cs
@@ -1,18 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
 
 public class PoliceCarEmission : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Light2D RedLight;
+    public Light2D BlueLight;
+    private int _State = 0;
+    float _IntensityValue = 0.1f;
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (_State == 0)
+        {
+            RedLight.intensity -= _IntensityValue;
+            if (RedLight.intensity <= 0)
+                _State = 1;
+        }
+
+        if (_State == 1)
+        {
+            BlueLight.intensity += _IntensityValue;
+            if (BlueLight.intensity >= 1)
+                _State = 2;
+        }
+
+        if (_State==2)
+        {
+            BlueLight.intensity -= _IntensityValue;
+            if (BlueLight.intensity <= 0)
+                _State = 3;
+        }
+
+        if(_State==3)
+        {
+            RedLight.intensity += _IntensityValue;
+            if (RedLight.intensity >= 1)
+                _State = 0;
+        }
     }
 }
