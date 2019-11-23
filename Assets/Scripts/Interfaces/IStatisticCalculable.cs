f4a0bdea7e247923ae0fd0849fdde97d6c47f8b4
Laszlo Bajusz <martenq1337@gmail.com>
2019-11-23 23:18:12 +0100

    Added: interfaces for calculate statistics

---
+++ Assets/Scripts/Interfaces/IStatisticCalculable.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.UI;
+
+namespace Assets.Scripts.Interfaces
+{
+    public interface IStatisticCalculable
+    {
+        void SetStatisticTexts(Text current, Text best, Text basic);
+        void SetNotChanginStats(int population, float mutationRate);
+        void SetChaningStats(int currentGeneration, int carsAlive, int currentFitness);
+    }
+}
---
+++ Assets/Scripts/Interfaces/IStatisticMeasurable.cs
@@ -0,0 +1,9 @@
+﻿namespace Assets.Scripts.Interfaces
+{
+    public interface IStatisticMeasurable
+    {
+        int ActualPopulation { get;}
+        int ActualGeneration { get;}
+        int BestFitness { get; }
+    }
+}
